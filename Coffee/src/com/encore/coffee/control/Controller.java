25ba2a0452825ae91475097571de8b8a367580ba
rockeedan <rockeedan1127@gmail.com>
2019-06-21 17:34:09 +0900

    여태까지 만든 뷰 Controller 에 붙임 (MainView, CofeeJoin, loginView, sell_g, sell update)

--- Coffee/src/com/encore/coffee/control/Controller.java
+++ Coffee/src/com/encore/coffee/control/Controller.java
@@ -5,6 +5,9 @@ import com.encore.coffree.view.MainView;
 import com.encore.coffree.view.MemberUp;
 import com.encore.coffree.view.coffeeloginView;
 import com.encore.coffree.view.update;
+import com.encore.coffree.view.sell_g;
+import com.encore.coffree.view.sell;
+
 
 public class Controller {
 	
@@ -12,10 +15,22 @@ public class Controller {
 	CoffeeJoin joinView;
 	coffeeloginView loginView;
 	MemberUp memberUp;
-
+	sell_g sell_g;
+	sell sell;
 	update adminUp;
 	
 	
+	public Controller() {
+		
+		mainView = new MainView();
+		joinView = new CoffeeJoin();
+		loginView = new coffeeloginView();
+		memberUp = new MemberUp();
+		sell_g = new sell_g();
+		sell = new sell();
+		adminUp = new update();
+		
+	}
 	
 	
 	public static void main(String[] args) {
