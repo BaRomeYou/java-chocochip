c67be4596a9b3589558ec23b939194e363d8a8c1
rockeedan <rockeedan1127@gmail.com>
2019-06-21 18:27:39 +0900

    coffeeDAO - 회원가입, 로그인 메소드 추가 memberVO -freq 잘못된 자료형 수정

--- Coffee/src/com/encore/coffee/model/CoffeeDAO.java
+++ Coffee/src/com/encore/coffee/model/CoffeeDAO.java
@@ -1,5 +1,119 @@
 package com.encore.coffee.model;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.sound.midi.Sequence;
+
 public class CoffeeDAO {
 
+	Connection conn;
+	// Statement stmt;
+	PreparedStatement stmt;
+	ResultSet rs;
+
+	Properties pro; // DB접속관련 정보 저장 객체
+
+	public CoffeeDAO() {
+		try {
+			pro = new Properties(); // 속성 담는 객체
+			pro.load(new FileReader("conn/conn.properties"));
+			// 속성 driver, url,user,password
+			Class.forName(pro.getProperty("driver"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	} // 생성자
+
+	private void connect() {
+		try {
+			conn = DriverManager.getConnection(pro.getProperty("url"), pro);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void disconnect() {
+		try {
+			if (conn != null)
+				conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public boolean findLogin(String id, String pass) {
+			connect();
+
+		
+
+		try {
+			String sql = "select count(*) from member where id=? and pass=?";
+			stmt = conn.prepareStatement(sql); // DB에 sql문 전송
+			stmt.setString(1, id);
+			stmt.setString(2, pass); // 물음표 수만큼 셋팅
+			// rs = stmt.executeQuery(sql);
+			
+			rs = stmt.executeQuery(); // 이미 위에서 sql 문 전송, 중복 전송 X
+			if (rs.next()) { // 어자피 1행 나오니까 하지만 습관적으로 if안에 넣어주는 것이 좋음
+				// rs.getInt("count(*)") ==> Error 함수명을 컬럼명으로 사용하지 못함
+				// rs.getInt("cnt") ==> O <- 별명 줬을 때만
+				memberVO vo = new memberVO();
+				int cnt = rs.getInt(1);
+				if (cnt > 0)
+					return true;
+					
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		
+	return false;
+	
+}
+	public Boolean create(memberVO vo) {
+		connect();
+		
+	try {
+		String sql = "insert into member (id,num, name, pwd, birth, gender, phone, mail, freq) values(?,?,?,?,?,?,?,?,?)";
+		stmt.executeUpdate();
+		stmt.setString(1,vo.getId());
+		stmt.setInt(2,vo.getNo());
+		stmt.setString(3,vo.getName());
+		stmt.setString(4,vo.getPwd());
+		stmt.setString(5,vo.getBirth());
+		stmt.setString(6,vo.getGender());
+		stmt.setString(7,vo.getPhone());
+		stmt.setString(8,vo.getMail());
+		stmt.setInt(9,vo.getFreq());
+		return true;
+	}catch (SQLException e) {
+		e.printStackTrace();
+	}finally {
+		disconnect();
+	}
+		return false;
+	}
+	
+	
+	
 }
--- Coffee/src/com/encore/coffee/model/memberVO.java
+++ Coffee/src/com/encore/coffee/model/memberVO.java
@@ -11,11 +11,13 @@ public class memberVO {
 	String gender;
 	String phone;
 	String mail;
-	String freq;
+	int freq;
 	
+	public memberVO() {
+		
+	}
 	
-	
-	public memberVO(String name, String pwd, String birth, String gender, String phone, String mail, String freq,
+	public memberVO(String name, String pwd, String birth, String gender, String phone, String mail, int freq,
 			String id, int no) {
 		
 		this.name = name;
@@ -78,10 +80,10 @@ public class memberVO {
 	public void setMail(String mail) {
 		this.mail = mail;
 	}
-	public String getFreq() {
+	public int getFreq() {
 		return freq;
 	}
-	public void setFreq(String freq) {
+	public void setFreq(int freq) {
 		this.freq = freq;
 	}
 
