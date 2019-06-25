ada8491821bb8632fe10cb953faa32f1ccb6548d
Playdata <Playdata@DESKTOP-7VMR9KI>
2019-06-25 16:47:11 +0900

    컨트롤러 쓰레드 구현입니다

--- Coffee/src/com/encore/coffee/control/Controller.java
+++ Coffee/src/com/encore/coffee/control/Controller.java
@@ -2,6 +2,9 @@ package com.encore.coffee.control;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Calendar;
+
+import javax.swing.ImageIcon;
 
 import com.encore.coffee.view.CoffeeJoin;
 import com.encore.coffee.view.MainView;
@@ -10,7 +13,7 @@ import com.encore.coffee.view.coffeeloginView;
 import com.encore.coffee.view.sell;
 import com.encore.coffee.view.sell_g;
 import com.encore.coffee.view.update;
-
+import com.encore.coffee.view.order;
 
 public class Controller implements ActionListener {
 	
@@ -21,6 +24,7 @@ public class Controller implements ActionListener {
 	sell_g sell_g;
 	sell sell;
 	update adminUp;
+	order odr;
 	
 	
 	public Controller() {
@@ -32,6 +36,7 @@ public class Controller implements ActionListener {
 		sell_g = new sell_g();
 		sell = new sell();
 		adminUp = new update();
+		odr=new order();
 		eventUp();
 	}
 	
@@ -62,16 +67,90 @@ public class Controller implements ActionListener {
 		adminUp.btnNewButton.addActionListener(this);
 		adminUp.btnNewButton_1.addActionListener(this);
 		
+		//order
+		
+		odr.btnNewButton_2.addActionListener(this);
+		odr.btnNewButton_3.addActionListener(this);
+		odr.button.addActionListener(this);
+		odr.button_1.addActionListener(this);
+		odr.button_10.addActionListener(this);
+		odr.button_11.addActionListener(this);
+		odr.button_12.addActionListener(this);
+		odr.button_13.addActionListener(this);
+		odr.button_14.addActionListener(this);
+		odr.button_15.addActionListener(this);
+		odr.button_16.addActionListener(this);
+		odr.button_17.addActionListener(this);
+		odr.button_2.addActionListener(this);
+		odr.button_21.addActionListener(this);
+		odr.button_22.addActionListener(this);
+		odr.button_23.addActionListener(this);
+		odr.button_3.addActionListener(this);
+		odr.button_4.addActionListener(this);
+		odr.button_5.addActionListener(this);
+		odr.button_6.addActionListener(this);
+		odr.button_7.addActionListener(this);
+		odr.button_8.addActionListener(this);
+		odr.button_9.addActionListener(this);
+		
 	}
 	
+	//로그인 버튼눌러서 회원정보가 확인되어서 order뷰가 뜰때 pop창에 생성되야할 클래스
+	class Pop extends Thread{
+		 public void run() {
+			 int num=1;
+			 int P=0;
+			 Calendar cal=Calendar.getInstance();
+			 String sec=null;
+			 String[] picture= {"event1","event2","event3"};
+			 
+			 while(true) {
+				 
+				 try {
+					sec=(cal.get(Calendar.SECOND)<10?"0":"")+cal.get(Calendar.SECOND);
+					 
+					 sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 num++;
+				 
+				 if(num%5==0) {
+					 if(P<5) {
+						 P++;
+						 
+						 if(P==5) {
+							 P=0;
+							 
+						 }
+						 
+						 odr.lblNewLabel_1.setIcon(new ImageIcon(order.class.getResource("../images/"+picture[P]+".jpg")));
+					 }
+					 
+				 }
+				 
+				 
+			 }
+			 
+		 }
+		
+		
+		
+		
+		
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
 	
 	public static void main(String[] args) {
 		new Controller();
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	
 }
