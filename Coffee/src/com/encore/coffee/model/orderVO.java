cbb88296dafe5a4e8118cc91cca565022a7e3e45
rockeedan <rockeedan1127@gmail.com>
2019-06-26 19:04:19 +0900

    adminVO 삭제, productVO로 수정

--- Coffee/src/com/encore/coffee/model/orderVO.java
+++ Coffee/src/com/encore/coffee/model/orderVO.java
@@ -2,39 +2,45 @@ package com.encore.coffee.model;
 
 public class orderVO {
 	
-	private String id; 
-	private int no;
-	private int cnt; 
-	private int price ;
-	private String date;
+	private String id; //고객아이디 
+	private	String menu_id; //메뉴아이디 
+	private String menu; // 메뉴이름
+	private int cnt ; //구매수량
+	private int price ; //가격 
+	private String date; //날짜 
 	
-		
-	public orderVO(String id, int no, int cnt, int price, String date) {
+	
+	
+	
+	
+	
+	
+	public orderVO(String id, String menu_id, String menu, int cnt, int price, String date) {
 		
 		this.id = id;
-		this.no = no;
+		this.menu_id = menu_id;
+		this.menu = menu;
 		this.cnt = cnt;
 		this.price = price;
 		this.date = date;
 	}
-	
-	
-	
-	orderVO (){
-		
-	}
-	
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	public int getNo() {
-		return no;
+	public String getMenu_id() {
+		return menu_id;
 	}
-	public void setNo(int no) {
-		this.no = no;
+	public void setMenu_id(String menu_id) {
+		this.menu_id = menu_id;
+	}
+	public String getMenu() {
+		return menu;
+	}
+	public void setMenu(String menu) {
+		this.menu = menu;
 	}
 	public int getCnt() {
 		return cnt;
@@ -54,13 +60,15 @@ public class orderVO {
 	public void setDate(String date) {
 		this.date = date;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "orderVO [id=" + id + ", no=" + no + ", cnt=" + cnt + ", price=" + price + ", date=" + date + "]";
+		return "orderVO [id=" + id + ", menu_id=" + menu_id + ", menu=" + menu + ", cnt=" + cnt + ", price=" + price
+				+ ", date=" + date + "]";
 	}
 	
-	
+		
+
 	
 	
 }
