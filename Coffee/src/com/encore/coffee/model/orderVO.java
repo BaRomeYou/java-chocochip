857abcf5beb50678020757a7441ac7410431298b
rockeedan <rockeedan1127@gmail.com>
2019-07-04 11:23:03 +0900

    oderVO /new exerd

--- Coffee/src/com/encore/coffee/model/orderVO.java
+++ Coffee/src/com/encore/coffee/model/orderVO.java
@@ -1,40 +1,71 @@
 package com.encore.coffee.model;
 
+import java.sql.Date;
+
 public class orderVO {
-	
 	private String id; //고객아이디 
-	private	String menu_id; //메뉴아이디 
+	private String purchase_id;
+	private Date date;
 	private String menu; // 메뉴이름
 	private int cnt ; //구매수량
-	private int price ; //가격 
-	private String date; //날짜 
+	
+	
+	public orderVO() {
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 	
 	
-	
-	
-	
-	public orderVO(String id, String menu_id, String menu, int cnt, int price, String date) {
-		
+	public orderVO(String id, String purchase_id, Date date, String menu, int cnt) {
+		super();
 		this.id = id;
-		this.menu_id = menu_id;
+		this.purchase_id = purchase_id;
+		this.date = date;
 		this.menu = menu;
 		this.cnt = cnt;
-		this.price = price;
+	}
+
+	
+
+
+	public orderVO(String id, String purchase_id, Date date) {
+		super();
+		this.id = id;
+		this.purchase_id = purchase_id;
 		this.date = date;
 	}
+
+	
+
+
+	public orderVO(String purchase_id, String menu, int cnt) {
+		super();
+		this.purchase_id = purchase_id;
+		this.menu = menu;
+		this.cnt = cnt;
+	}
+
+
+
+
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	public String getMenu_id() {
-		return menu_id;
+	public String getPurchase_id() {
+		return purchase_id;
 	}
-	public void setMenu_id(String menu_id) {
-		this.menu_id = menu_id;
+	public void setPurchase_id(String purchase_id) {
+		this.purchase_id = purchase_id;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
 	}
 	public String getMenu() {
 		return menu;
@@ -48,24 +79,20 @@ public class orderVO {
 	public void setCnt(int cnt) {
 		this.cnt = cnt;
 	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	
+
+
+
+
 	@Override
 	public String toString() {
-		return "orderVO [id=" + id + ", menu_id=" + menu_id + ", menu=" + menu + ", cnt=" + cnt + ", price=" + price
-				+ ", date=" + date + "]";
+		return "orderVO [id=" + id + ", purchase_id=" + purchase_id + ", date=" + date + ", menu=" + menu + ", cnt="
+				+ cnt + "]";
 	}
+
+ 
+	
+	
+	
 	
 		
 
